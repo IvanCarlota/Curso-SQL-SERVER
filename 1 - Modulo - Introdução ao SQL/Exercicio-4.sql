bcc498562f2a2cafa0e2d2e3c1ad44d66e2516fa
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2023-01-04 20:50:00 -0300

    Usando a instrução select distinct para buscar todos os fornecedores

---
+++ 1 - Modulo - Introdução ao SQL/Exercicio-4.sql
@@ -0,0 +1,10 @@
+/* Exercicio 4 
+ A empresa Contoso precisa fazer contato com os fornecedores de produtos para repor o
+estoque. Você é da área de compras e precisa descobrir quem são esses fornecedores.
+Utilize um comando em SQL para retornar apenas os nomes dos fornecedores na tabela
+dimProduct e renomeie essa nova coluna da tabela. */
+
+select 
+	distinct Manufacturer AS 'Fornecedor'
+from 
+	DimProduct
