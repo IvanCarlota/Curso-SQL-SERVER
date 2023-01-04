e3542bba36f65e066cf22ab4d3efb2c344642dbb
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2023-01-04 20:51:40 -0300

    Fazendo mais de uma consulta ao mesmo tempo no sql server

---
+++ 1 - Modulo - Introdução ao SQL/Exercicio-5.sql
@@ -0,0 +1,12 @@
+/*Exercicio 5
+. O seu trabalho de investigação não para. Você precisa descobrir se existe algum produto
+registrado na base de produtos que ainda não tenha sido vendido. Tente chegar nessa
+informação.
+Obs: caso tenha algum produto que ainda não tenha sido vendido, você não precisa descobrir
+qual é, é suficiente saber se teve ou não algum produto que ainda não foi vendido. */
+
+select * from DimProduct
+
+select top (1000) * from FactSales
+
+select distinct productkey from factSales
